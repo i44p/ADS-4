62e9ee2a7e9a11e72e1ac3dd7bf1a8a9efc42e4c
i44p <jsbbdjzjsjawk+@gmail.com>
2023-03-14 13:31:28 +0300

    implement countPairs1, countPairs2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,27 @@
 // Copyright 2021 NNTU-CS
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++)
+    for (int j = i + 1; j < len; j++)
+      if (arr[i] + arr[j] == value) count++;
+  return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    if (i >= value) continue;
+    for (int j = i + 1; j < len; j++) {
+      int s = arr[i] + arr[j];
+      if (s == value) {
+        count++;
+        continue;
+      }
+      if (s > value) break;
+    }
+  }
+  return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+
+int countPairs3(int *arr, int len, int value) { return 0; }
